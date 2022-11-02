1a00d037676450715e920a992395113e4e48c904
Anshay Saboo <anshaysaboo1586@gmail.com>
2022-11-01 19:54:19 -0700

    Renamed AWS_ACCESS_KEY_ID environment variable

--- src/util/awsUtil.js
+++ src/util/awsUtil.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 
 const s3Client = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
   region: "us-west-1",
 });
