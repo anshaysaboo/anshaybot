cd998190f5884572df1abe8ed51378669c4d52b9
Anshay Saboo <anshaysaboo1586@gmail.com>
2023-02-08 17:15:36 -0800

    Push for redeploy

--- src/middleware/reactionAdded.js
+++ src/middleware/reactionAdded.js
@@ -2,7 +2,6 @@ const { quoteReaction } = require("../actions/quotes.js");
 
 module.exports = async (data) => {
   const { event } = data;
-
   try {
     if (event.reaction === "writing_hand") {
       await quoteReaction(data);
